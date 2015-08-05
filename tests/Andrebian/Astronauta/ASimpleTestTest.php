f84514c7417ae32c91a7d14f0be2bfc2c24b2bba
Andre Cardozo <andre@Mac-mini-de-fouritil02.local>
2015-08-05 16:29:33 -0300

    Adicionado um teste para o PHPCI

---
+++ tests/Andrebian/Astronauta/ASimpleTestTest.php
@@ -0,0 +1,20 @@
+<php
+
+namespace Andrebian\Astronauta;
+
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+
+class ASimpleTestTest extends PHPUnit
+{
+	
+	/**
+	* Apenas para testar a integração com o PHPCI
+	*/
+	public function testOne()
+	{
+		$this->assertTrue(true);
+	}
+
+}
